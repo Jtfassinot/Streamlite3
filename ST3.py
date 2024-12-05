420dfe2d5c2725247a22e1e7381256927a7ec65f
Jtfassinot <fassinot.jt@gmail.com>
2024-12-05 18:49:01 +0100

    Update ST3.py
    définitif

--- ST3.py
+++ ST3.py
@@ -27,40 +27,45 @@ authenticator = Authenticate(
 authenticator.login()
 
 def accueil():
-      st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
+      st.title("Bienvenu sur le contenu réservé aux pilotes émérites")
         # Création du menu qui va afficher les choix qui se trouvent dans la variable options
-selection = option_menu(
+
+ # Ajout d'une barre latérale
+with st.sidebar:
+    st.button("Déconnexion")
+
+    selection = option_menu(
             menu_title=None,
             options = ["Accueil", "Photos"]
         )
 
 # On indique au programme quoi faire en fonction du choix
 if selection == "Accueil":
+
+    st.write("Bienvenue **root**")
     st.write("Bienvenue sur la page d'accueil !")
     st.header("La futur !!! version améliorer, chassis plus perfomant, angle de chasse modifié pour etre plus sur l'avant")
     st.header("Elle est fin prete pour la piste")
-    st.image("/Users/julien-thomasfassinot/Documents/WCS/VSD/Streamlit partie 2/MT09.jpeg", caption="Voici une belle photo", use_container_width=True)
+    st.image("/Users/julien-thomasfassinot/Documents/WCS/VSD/Streamlit partie 2/MT09.jpeg", caption="Voici la bete", use_container_width=True)
 
 
 elif selection == "Photos":
 
-    import streamlit as st
-
-    st.write("Bienvenue sur mon album photo")
+    st.write("Bienvenue sur la page des 'tous motards' ")
 
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.header("Polyvalente")
-        st.image("https://cdn2.yamaha-motor.eu/prod/product-assets/2024/MT09DX/2024-Yamaha-MT09DX-EU-Icon_Performance-360-Degrees-001-03.jpg", caption="Voici une belle photo", use_container_width=True)
+        st.header("numero uno")
+        st.image("https://cdn2.yamaha-motor.eu/prod/product-assets/2024/MT09DX/2024-Yamaha-MT09DX-EU-Icon_Performance-360-Degrees-001-03.jpg", caption="Voici la bete", use_container_width=True)
 
     with col2:
-        st.header("Pour la piste")
-        st.image("https://cdn2.yamaha-motor.eu/prod/product-assets/2024/MT09DX/2024-Yamaha-MT09DX-EU-Icon_Performance-360-Degrees-001-03.jpg", caption="Voici une belle photo", use_container_width=True)
+        st.header("le tracteur")
+        st.image("https://www.ktmmalaysia.com/wp-content/uploads/2021/03/PHO_BIKE_90_RE_1290-sdr-21-or-90re_SALL_AEPI_V1.png", caption="Voici la fusée", use_container_width=True)
 
     with col3:
-        st.header("Streetfighter V4")
-        st.image("https://cdn2.yamaha-motor.eu/prod/product-assets/2024/MT09DX/2024-Yamaha-MT09DX-EU-Icon_Performance-360-Degrees-001-03.jpg", caption="Voici une belle photo", use_container_width=True)
+        st.header("L'A2")
+        st.image("https://storage.kawasaki.eu/public/kawasaki.eu/en-EU/model/16ER650F_44SBEIDRS2CG_C.png", caption="Voici celle qui prend le plus d'angle", use_container_width=True)
 
 if st.session_state["authentication_status"]:
   accueil()
